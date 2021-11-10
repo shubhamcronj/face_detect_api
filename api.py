951652dd86537fe85119d7137e75d0fabd80e727
Shubham <shubham@cronj.com>
2021-11-10 18:52:10 +0530

    solved cors

--- api.py
+++ api.py
@@ -1,15 +1,19 @@
 import numpy as np
 import cv2
 from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
 import base64
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
+CORS(app, support_credentials=True)
 
-@app.route('/detect_face', methods=['POST'])
+
+@app.route('/detect_face', methods=['POST', 'OPTIONS'])
+@cross_origin(supports_credentials=True)
 def detect_face():
     content = request.json
-    print(content)
+    #print(content)
     haar_face_cascade = cv2.CascadeClassifier('data/haarcascade_frontalface_alt.xml')
     nparr = np.fromstring(base64.b64decode(content["base64"]), np.uint8)
     test1 = cv2.imdecode(nparr, cv2.IMREAD_ANYCOLOR)
@@ -19,4 +23,4 @@ def detect_face():
     print('Faces found: ', len(faces))
     return jsonify({"status":"OK", "faces": len(faces)})
 
-app.run()
+app.run(host='0.0.0.0', port="6666")
